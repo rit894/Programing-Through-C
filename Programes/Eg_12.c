52b78d107be34e415e0e67cf709a5ba001a6107c
rit894 <nagarithwikv@gmail.com>
2026-03-03 17:54:58 +0530

    Implement string manipulation with integer offsets in Eg_12.c

--- Programes/Eg_12.c
+++ Programes/Eg_12.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
+
+    printf("Enter the string: ");
+    char string[20];
+    scanf("%s", &string);
+    printf("Enter the Number(same size as String): ");
+    int integers[20];
+    int length = strlen(string);
+    for(int i =0; i<length;i++){
+        scanf("%d", &integers[i]);
+    }
+    for(int i =0; i<length;i++){
+        char c = string[i];
+        int n = integers[i];
+        c=c+n;
+        string[i] = c;
+        printf("%c",c);
+    }
+    
+        
+    
+
 
 
 
